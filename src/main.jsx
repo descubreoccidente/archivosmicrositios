b29f3251d160655e3f6e2426ff5350af12e18c36
Carlos <carbelaezl@gmail.com>
2026-08-06 11:56:10 -0500

    Corregir imports de CSS en main.jsx para activar Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import './styles (CSS)/tailwind.css'
+import './styles (CSS)/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        terracota: '#B85A3A',
+        terracota: '#b34127',
         'terracota-dark': '#8C3E25',
-        marron: '#2C1810',
+        naranja: '#f26631',
+        teal: '#006468',
+        magenta: '#e20d77',
         crema: '#F7F0E8',
-        dorado: '#D4A017',
-        gris: '#6B5B52'
-      }
+        marron: '#2C1810',
+        gris: '#6B5B52',
+      },
+      fontFamily: {
+        primaria: ['Fredoka', 'sans-serif'],
+        secundaria: ['Barlow', 'sans-serif'],
+      },
     },
   },
   plugins: [],
